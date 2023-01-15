fed18b828448d10d5c1c79feccb4ba53a3b18d33
Joe <caspar0912@googlemail.com>
2023-01-15 23:43:44 +0000

    add magnifier package

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -3,6 +3,7 @@ import { Container } from '@mantine/core';
 import { useParams } from 'react-router-dom';
 import { useAppSelector } from '../../redux/hooks/hooks';
 import { RootState } from '../../redux/store';
+import { GlassMagnifier } from 'react-image-magnifiers';
 
 const Game = () => {
   const params = useParams();
@@ -12,7 +13,17 @@ const Game = () => {
   console.log(game);
   return (
     <main>
-      <Container></Container>
+      <Container>
+        <GlassMagnifier
+        style={{height: 'auto', width: 'auto'}}
+          imageSrc={game.imageSrc}
+          largeImageSrc={game.imageSrc}
+          imageAlt={game.name}
+          magnifierSize={'10%'}
+          magnifierBorderColor='black'
+          square
+        />
+      </Container>
     </main>
   );
 };
