edd1939a455e74fb0c1382262ebf33989d39b92c
Joe <caspar0912@googlemail.com>
2023-01-16 13:22:36 +0000

    add large images and start logic to find image positions

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from '@mantine/core';
+import { Box, Container } from '@mantine/core';
 import { useParams } from 'react-router-dom';
 import { useAppSelector } from '../../redux/hooks/hooks';
 import { RootState } from '../../redux/store';
@@ -10,19 +10,27 @@ const Game = () => {
   const [game] = useAppSelector((state: RootState) =>
     state.game.games.filter((game) => game.id === params.id)
   );
-  console.log(game);
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const node = e.target as HTMLElement;
+    const x = (e.nativeEvent.offsetX / node.offsetWidth) * 100;
+    const y = (e.nativeEvent.offsetY / node.offsetHeight) * 100;
+    console.log(x, y);
+  };
+
   return (
     <main>
       <Container>
-        <GlassMagnifier
-        style={{height: 'auto', width: 'auto'}}
-          imageSrc={game.imageSrc}
-          largeImageSrc={game.imageSrc}
-          imageAlt={game.name}
-          magnifierSize={'10%'}
-          magnifierBorderColor='black'
-          square
-        />
+        <Box onClick={(e) => handleClick(e)}>
+          <GlassMagnifier
+            imageSrc={game.imageSrc}
+            largeImageSrc={game.imageSrcLg}
+            imageAlt={game.name}
+            magnifierSize={'10%'}
+            magnifierBorderColor="black"
+            square
+          />
+        </Box>
       </Container>
     </main>
   );
--- src/redux/slices/gameData.ts
+++ src/redux/slices/gameData.ts
@@ -1,19 +1,23 @@
 import game1 from '../../assets/images/planetiso.webp';
+import game1lg from '../../assets/images/planetisolg.webp';
 import game1char1 from '../../assets/images/game1-claptrap.png';
 import game1char2 from '../../assets/images/game1-wario.png';
 import game1char3 from '../../assets/images/game1-master-chief.png';
 
 import game2 from '../../assets/images/isowrd-and-isorcery.webp';
+import game2lg from '../../assets/images/isowrd-and-isorcerylg.webp';
 import game2char1 from '../../assets/images/game2-geralt.png';
 import game2char2 from '../../assets/images/game2-donkey.png';
 import game2char3 from '../../assets/images/game2-willow.webp';
 
 import game3 from '../../assets/images/prehisoria.webp';
+import game3lg from '../../assets/images/prehisorialg.webp';
 import game3char1 from '../../assets/images/game3-sonic.png';
 import game3char2 from '../../assets/images/game3-marty-mcfly.png';
 import game3char3 from '../../assets/images/game3-predator.png';
 
 import game4 from '../../assets/images/pirates-of-the-isobbean.webp';
+import game4lg from '../../assets/images/pirates-of-the-isobbeanlg.webp';
 import game4char1 from '../../assets/images/game4-link.png';
 import game4char2 from '../../assets/images/game4-inigo-montoya.png';
 import game4char3 from '../../assets/images/game4-monkey-d-luffy.png';
@@ -26,6 +30,7 @@ interface character {
 interface game {
   name: string;
   imageSrc: string;
+  imageSrcLg: string;
   id: string;
   characters: Array<character>;
 }
@@ -41,6 +46,7 @@ export const initialState: gameState = {
     {
       name: 'Planet ISO',
       imageSrc: game1,
+      imageSrcLg: game1lg,
       id: 'game1',
       characters: [
         {
@@ -60,6 +66,7 @@ export const initialState: gameState = {
     {
       name: 'ISOwrd & ISOrcery',
       imageSrc: game2,
+      imageSrcLg: game2lg,
       id: 'game2',
       characters: [
         {
@@ -79,6 +86,7 @@ export const initialState: gameState = {
     {
       name: 'PrehISOria',
       imageSrc: game3,
+      imageSrcLg: game3lg,
       id: 'game3',
       characters: [
         {
@@ -98,6 +106,7 @@ export const initialState: gameState = {
     {
       name: 'Pirates of the ISObbean',
       imageSrc: game4,
+      imageSrcLg: game4lg,
       id: 'game4',
       characters: [
         {
